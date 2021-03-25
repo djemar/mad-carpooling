4c196dd16a1c734b5d3bc159b88a86c4cf10c532
Diego Marino <marinodiego.96@gmail.com>
2021-03-25 08:55:19 +0100

    create ShowProfileActivity

---
+++ app/src/main/java/com/mad/carpooling/ShowProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.mad.carpooling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_profile)
+    }
+}
