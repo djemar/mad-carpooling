80481dfba278dcb18b7f9e63d192ca4ee8cfaf04
Diego Marino <marinodiego.96@gmail.com>
2021-04-17 16:14:33 +0200

    init drawer header at launch

--- app/src/main/java/com/mad/carpooling/MainActivity.kt
+++ app/src/main/java/com/mad/carpooling/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.mad.carpooling
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,6 +39,31 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        initDrawerHeader(navView)
+    }
+
+    private fun initDrawerHeader(navView: NavigationView) {
+        val headerView: View = navView.getHeaderView(0)
+        val ivProfileHeader: ImageView = headerView.findViewById(R.id.nav_header_profile_pic)
+        val tvFullNameHeader: TextView = headerView.findViewById(R.id.nav_header_fullName)
+        val tvNicknameHeader: TextView = headerView.findViewById(R.id.nav_header_nickname)
+        val sharedPref = getSharedPreferences("profile_pref.group05.lab1", Context.MODE_PRIVATE) ?: return
+        val jsonString = sharedPref.getString(getString(R.string.saved_profile_data), null)
+        if (jsonString != null) {
+            val jsonObject = JSONObject(jsonString)
+            tvFullNameHeader.text = jsonObject.getString("json_fullName.group05.lab1")
+            tvNicknameHeader.text = jsonObject.getString("json_nickname.group05.lab1")
+            ivProfileHeader.setImageBitmap(
+                BitmapFactory.decodeStream(
+                    openFileInput(
+                        jsonObject.getString(
+                            "json_profilePic.group05.lab1"
+                        )
+                    )
+                )
+            )
+        }
     }
 
 /*    override fun onCreateOptionsMenu(menu: Menu): Boolean {
