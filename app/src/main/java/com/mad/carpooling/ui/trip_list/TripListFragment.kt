c063e1aa0b4a1e2d0e11ca68dbb2e4306a03d8e8
Diego Marino <marinodiego.96@gmail.com>
2021-04-23 16:27:01 +0200

    define Trip class

--- app/src/main/java/com/mad/carpooling/ui/trip_list/TripListFragment.kt
+++ app/src/main/java/com/mad/carpooling/ui/trip_list/TripListFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
@@ -23,13 +19,18 @@ import com.mad.carpooling.R
 
 class TripListFragment : Fragment(R.layout.fragment_trip_list) {
 
+    companion object {
+        var idN: Int = 0
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val rv = view.findViewById<RecyclerView>(R.id.triplist_rv)
         rv.layoutManager = LinearLayoutManager(context)
         //just an example, real trips needed
-        val triplist = (1..10).map { Trip("From_to $it", "duration $it", "price $it") }.toMutableList()
+        val triplist =
+            (1..10).map { Trip(idN++) }.toMutableList()
         //val triplist = listOf<Trip>() //to check emptyView
         val tripAdapter = TripAdapter(triplist)
         rv.adapter = tripAdapter
@@ -49,20 +50,36 @@ class TripListFragment : Fragment(R.layout.fragment_trip_list) {
                 if (dy > 0 && fab.getVisibility() === View.VISIBLE) {
                     fab.hide()
                 } else if (dy < 0 && fab.getVisibility() !== View.VISIBLE) {
-                   fab.show()
+                    fab.show()
                 }
             }
         })
     }
 }
 
-class Trip(val location: String, val duration: String, val price: String){
+class Trip(
+    val id: Int = 1,
+    val nickname: String = "Babayaga",
+    val locationFrom: String = "From x",
+    val locationTo: String = "To y",
+    val duration: String = "hh:mm",
+    val date: String = "dd-mm-yyyy",
+    val time: String = "hh:mm",
+    val seats: Int = 1,
+    val price: Float = 1f,
+    val chattiness: Boolean = false,
+    val smoking: Boolean = false,
+    val pets: Boolean = false,
+    val music: Boolean = false,
+    val description: String = "Description",
+    val stops: HashMap<Int, String>? = null
+) {
 
 }
 
-class TripAdapter(val triplist: List<Trip>): RecyclerView.Adapter<TripAdapter.TripViewHolder>(){
+class TripAdapter(val triplist: List<Trip>) : RecyclerView.Adapter<TripAdapter.TripViewHolder>() {
 
-    class TripViewHolder(v: View): RecyclerView.ViewHolder(v){
+    class TripViewHolder(v: View) : RecyclerView.ViewHolder(v) {
 
         val tripRL = v.findViewById<RelativeLayout>(R.id.trip_rl)
         val location = v.findViewById<TextView>(R.id.trip_from_to)
@@ -72,13 +89,13 @@ class TripAdapter(val triplist: List<Trip>): RecyclerView.Adapter<TripAdapter.Tr
 
         var navController: NavController? = null
 
-        fun bind(trip: Trip){
+        fun bind(trip: Trip) {
 
-            location.text = trip.location
+            location.text = "${trip.locationFrom} ${trip.locationTo}"
             duration.text = trip.duration
-            price.text = trip.price
+            price.text = trip.price.toString()
 
-            tripRL.setOnClickListener{
+            tripRL.setOnClickListener {
                 navController = Navigation.findNavController(tripRL)
                 navController!!.navigate(R.id.action_nav_trip_list_to_nav_trip_details)
             }
@@ -91,7 +108,8 @@ class TripAdapter(val triplist: List<Trip>): RecyclerView.Adapter<TripAdapter.Tr
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TripViewHolder {
-        val layout = LayoutInflater.from(parent.context).inflate(R.layout.triplist_layout, parent, false)
+        val layout =
+            LayoutInflater.from(parent.context).inflate(R.layout.triplist_layout, parent, false)
         return TripViewHolder(layout)
     }
 
