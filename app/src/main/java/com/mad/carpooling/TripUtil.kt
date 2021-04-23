29e73af7020289de8288ab96b3326cbef0e485c2
Diego Marino <marinodiego.96@gmail.com>
2021-04-23 18:57:45 +0200

    update TripUtil

--- app/src/main/java/com/mad/carpooling/TripUtil.kt
+++ app/src/main/java/com/mad/carpooling/TripUtil.kt
@@ -1,25 +1,29 @@
 package com.mad.carpooling
 
-class Trip(
-    val id: Int = 1,
-    val nickname: String = "Babayaga",
-    val locationFrom: String = "From x",
-    val locationTo: String = "To y",
-    val duration: String = "hh:mm",
-    val date: String = "dd-mm-yyyy",
-    val time: String = "hh:mm",
-    val seats: Int = 1,
-    val price: Float = 1f,
-    val chattiness: Boolean = false,
-    val smoking: Boolean = false,
-    val pets: Boolean = false,
-    val music: Boolean = false,
-    val description: String = "Description",
-    val stops: HashMap<Int, String>? = null
+class TripUtil(
+
 ) {
     companion object {
         var idN: Int = 0
     }
+
+    class Trip(
+        val id: Int = 1,
+        val nickname: String = "Babayaga",
+        val departure: String = "From x",
+        val arrival: String = "To y",
+        val duration: String = "hh:mm",
+        val depDate: String = "dd-mm-yyyy",
+        val depTime: String = "hh:mm",
+        val seats: Int = 1,
+        val price: Float = 1f,
+        val chattiness: Boolean = false,
+        val smoking: Boolean = false,
+        val pets: Boolean = false,
+        val music: Boolean = false,
+        val description: String = "Description",
+        val stops: HashMap<Int, String>? = hashMapOf(1 to "x", 2 to "y", 3 to "zz")
+    ) {}
 
     private val triplist =
         (1..10).map { Trip(idN++) }.toMutableList()
@@ -32,11 +36,11 @@ class Trip(
         return triplist
     }
 
-    fun updateTrip(id: Int, trip : Trip){
+    fun updateTrip(id: Int, trip: Trip) {
         triplist[id] = trip
     }
 
-    fun addTrip(trip: Trip){
+    fun addTrip(trip: Trip) {
         triplist.add(trip)
     }
 }
