4f69aba56d73072a8b902e77cc581e92640b26ad
Diego Marino <marinodiego.96@gmail.com>
2021-04-27 10:41:01 +0200

    add 2 fake trips to display

--- app/src/main/java/com/mad/carpooling/TripUtil.kt
+++ app/src/main/java/com/mad/carpooling/TripUtil.kt
@@ -22,25 +22,72 @@ class TripUtil(
         val pets: Boolean = false,
         val music: Boolean = false,
         val description: String = "Description",
-        val stops: HashMap<Int, String>? = hashMapOf(0 to "Florence, 18/04/21, 3h", 1 to "Siena, 18/04/21, 1h", 2 to "Pisa, 18/04/21, 1h")
+        val stops: HashMap<Int, String>? = hashMapOf(
+            0 to "Florence, 18/04/21, 3h",
+            1 to "Siena, 18/04/21, 1h",
+            2 to "Pisa, 18/04/21, 1h"
+        )
     ) {}
 
-    private val triplist =
-        (1..10).map { Trip(idN++) }.toMutableList()
+    private val tripList = mutableListOf<Trip>(
+        Trip(
+            1,
+            "Babayaga",
+            "Torino",
+            "Ivrea",
+            "01:00",
+            "28-04-2021",
+            "07:00",
+            4,
+            25f,
+            false,
+            false,
+            true,
+            false,
+            "It will be a killer of a trip!",
+            hashMapOf(
+                0 to "Torino, 18/04/21, 10min",
+                1 to "Rivoli, 18/04/21, 40min",
+                2 to "Ivrea, 18/04/21, 10min"
+            )
+        ),
+        Trip(
+            2,
+            "MrWinston",
+            "Calascibetta",
+            "Bari",
+            "06:00",
+            "28-04-2024",
+            "05:00",
+            1,
+            45f,
+            true,
+            false,
+            false,
+            true,
+            "Calascibetta caput mundi.",
+            hashMapOf(
+                0 to "Calascibetta, 18/04/24, 3h",
+                1 to "Messina, 18/04/24, 1h",
+                2 to "Reggio, 18/04/24, 1h",
+                3 to "Bari, 18/04/24, 1h",
+            )
+        )
+    )
 
     fun getTrip(id: Int): Trip {
-        return triplist[id]
+        return tripList[id]
     }
 
     fun getTripList(): MutableList<Trip> {
-        return triplist
+        return tripList
     }
 
     fun updateTrip(id: Int, trip: Trip) {
-        triplist[id] = trip
+        tripList[id] = trip
     }
 
     fun addTrip(trip: Trip) {
-        triplist.add(trip)
+        tripList.add(trip)
     }
 }
