08e10cc075039223b289235f05d5b326b3e01f61
Enrico D'Oro <er.enricodoro@gmail.com>
2021-05-02 11:43:01 +0200

    implement TripEditViewModel

--- app/src/main/java/com/mad/carpooling/ui/trip_edit/TripEditViewModel.kt
+++ app/src/main/java/com/mad/carpooling/ui/trip_edit/TripEditViewModel.kt
@@ -3,11 +3,18 @@ package com.mad.carpooling.ui.trip_edit
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mad.carpooling.Trip
 
 class TripEditViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+    private lateinit var trip : Trip
+
+    fun getTrip() : Trip{
+        return trip
     }
-    val text: LiveData<String> = _text
+
+    fun setTrip(trip: Trip){
+        this.trip = trip
+    }
+
 }
