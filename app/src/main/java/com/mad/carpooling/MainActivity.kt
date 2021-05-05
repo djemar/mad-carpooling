54a79cc08e4342a3fb77ee84781274b8d9e2f22e
Diego Marino <marinodiego.96@gmail.com>
2021-05-05 14:29:09 +0200

    add firebaseui login support

--- app/src/main/java/com/mad/carpooling/MainActivity.kt
+++ app/src/main/java/com/mad/carpooling/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.mad.carpooling
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,19 +17,29 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var auth: FirebaseAuth
+    private val RC_SIGN_IN : Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
         val toolbar: Toolbar = findViewById(R.id.my_toolbar)
         setSupportActionBar(toolbar)
-        
+
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -43,21 +56,79 @@ class MainActivity : AppCompatActivity() {
         initDrawerHeader(navView)
     }
 
+    private fun login() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    private fun logout(){
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // TODO logout
+            }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            login()
+        }
+    }
+
     private fun initDrawerHeader(navView: NavigationView) {
         val headerView: View = navView.getHeaderView(0)
         val ivProfileHeader: ImageView = headerView.findViewById(R.id.nav_header_profile_pic)
         val tvFullNameHeader: TextView = headerView.findViewById(R.id.nav_header_fullName)
         val tvNicknameHeader: TextView = headerView.findViewById(R.id.nav_header_nickname)
-        val sharedPref = getSharedPreferences("profile_pref.group05.lab1", Context.MODE_PRIVATE) ?: return
+        val sharedPref =
+            getSharedPreferences("profile_pref.group05.lab1", Context.MODE_PRIVATE) ?: return
         val jsonString = sharedPref.getString(getString(R.string.saved_profile_data), null)
         if (jsonString != null) {
             val jsonObject = JSONObject(jsonString)
             tvFullNameHeader.text = jsonObject.getString("json_fullName.group05.lab1")
             tvNicknameHeader.text = jsonObject.getString("json_nickname.group05.lab1")
-            BitmapFactory.decodeFile(jsonObject.getString(
-                "json_profilePic.group05.lab1"
-            ))?.also { bitmap ->
+            BitmapFactory.decodeFile(
+                jsonObject.getString(
+                    "json_profilePic.group05.lab1"
+                )
+            )?.also { bitmap ->
                 ivProfileHeader.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // add to user db, check if exists
+                //user.uid
+                Log.d("Login result", "Sign in success")
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Log.e("Login result", "Sign in failed")
             }
         }
     }
