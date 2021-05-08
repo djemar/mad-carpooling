c9a89861460d68b71b78194bd5e66299ae43f7d6
Diego Marino <marinodiego.96@gmail.com>
2021-05-09 00:34:44 +0200

    setup search view

--- app/src/main/java/com/mad/carpooling/ui/others_trip_list/OthersTripListFragment.kt
+++ app/src/main/java/com/mad/carpooling/ui/others_trip_list/OthersTripListFragment.kt
@@ -7,21 +7,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.core.content.ContextCompat
+import android.view.*
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.android.material.slider.RangeSlider
 import com.mad.carpooling.MainActivity
 import com.mad.carpooling.R
 import com.mad.carpooling.data.Trip
@@ -31,16 +40,32 @@ private lateinit var auth: FirebaseAuth
 
 class OthersTripListFragment : Fragment(R.layout.fragment_trip_list) {
     private lateinit var rv: RecyclerView
+    private lateinit var appBarLayout: AppBarLayout
+    private lateinit var sliderPrice: RangeSlider
+    private lateinit var tvSliderPrice: TextView
     private var tripMap: HashMap<String, Trip>? = null
     private val model: SharedViewModel by activityViewModels()
 
     // Use the 'by activityViewModels()' Kotlin property delegate
     // from the fragment-ktx artifact
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        sliderPrice = (activity as MainActivity).findViewById(R.id.range_slider)
+        tvSliderPrice = (activity as MainActivity).findViewById(R.id.tv_price_slider)
+        sliderPrice.values = mutableListOf(10f, 50f)
+        sliderPrice.addOnChangeListener { slider, value, fromUser ->
+            tvSliderPrice.text = "${slider.values[0]} - ${slider.values[1]} €"
+        }
+        appBarLayout = (activity as MainActivity).findViewById(R.id.appbar_layout) as AppBarLayout
+        appBarLayout.setExpanded(false) //TODO disable expansion in other fragments
         model.getCurrentUser().observe(viewLifecycleOwner, Observer { currentUser ->
             // update after login/logout
             model.getOthersTrips().observe(viewLifecycleOwner, Observer { newTripsMap ->
@@ -52,11 +77,10 @@ class OthersTripListFragment : Fragment(R.layout.fragment_trip_list) {
     }
 
 
-
     private fun updateTripList(tripsMap: HashMap<String, Trip>, view: View) {
         rv = view.findViewById<RecyclerView>(R.id.triplist_rv)
         rv.layoutManager = LinearLayoutManager(context)
-        //just an example, real trips needed
+        rv.isNestedScrollingEnabled = false; //prevent toolbar to expand on scroll
 
         val tripAdapter = OthersTripAdapter(ArrayList((tripsMap.values)))
         rv.adapter = tripAdapter
@@ -87,6 +111,31 @@ class OthersTripListFragment : Fragment(R.layout.fragment_trip_list) {
                 }
             }
         })
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main, menu)
+        // optionsMenu = menu
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_search -> {
+                appBarLayout.setExpanded((appBarLayout.height - appBarLayout.bottom) != 0, true)
+                if ((appBarLayout.height - appBarLayout.bottom) == 0)
+                    initSearch()
+                true
+            }
+            else -> item.onNavDestinationSelected(findNavController()) || super.onOptionsItemSelected(
+                item
+            )
+        }
+    }
+
+    private fun initSearch() {
+
     }
 
     class OthersTripAdapter(private val tripList: ArrayList<Trip>) :
@@ -137,6 +186,11 @@ class OthersTripListFragment : Fragment(R.layout.fragment_trip_list) {
                 Navigation.findNavController(holder.tripRL).navigate(action)
             }
             holder.btnStar.visibility = View.VISIBLE
+            holder.btnStar.setOnClickListener {
+                val action =
+                    OthersTripListFragmentDirections.actionNavOthersTripListToNavTripDetails(
+                        trip.id,
+                    )
             holder.btnStar.setOnCheckedChangeListener { it, isChecked ->
                 if (isChecked) {
                     db.collection("trips").document(trip.id).update(
