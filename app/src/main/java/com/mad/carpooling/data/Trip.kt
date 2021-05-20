a9b7557f69e8dfd900c69b2ad72e961149ebf8e5
Diego Marino <marinodiego.96@gmail.com>
2021-05-20 11:47:48 +0200

    add geopoints to trip model

--- app/src/main/java/com/mad/carpooling/data/Trip.kt
+++ app/src/main/java/com/mad/carpooling/data/Trip.kt
@@ -2,6 +2,7 @@ package com.mad.carpooling.data
 
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.GeoPoint
 
 
 data class Trip(
@@ -18,10 +19,11 @@ data class Trip(
     var pets: Boolean = false,
     var music: Boolean = false,
     var description: String = "",
-    var stops: ArrayList<String>? = java.util.ArrayList<String>(),
+    var stops: ArrayList<String>? = ArrayList<String>(),
     var imageCarURL: String? = null, //TODO change also in DB to imageCarRef
-    var interestedPeople: ArrayList<String>? = java.util.ArrayList<String>(),
-    var acceptedPeople: ArrayList<String>? = java.util.ArrayList<String>(),
+    var interestedPeople: ArrayList<String>? = ArrayList<String>(),
+    var acceptedPeople: ArrayList<String>? = ArrayList<String>(),
+    var geopoints: ArrayList<GeoPoint> = ArrayList<GeoPoint>(),
     var visibility: Boolean = true
 ) {
 }
