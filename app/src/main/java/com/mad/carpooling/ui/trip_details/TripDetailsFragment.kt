62495283bcac9c7ab666dd2138c53f01ec89029a
Diego Marino <marinodiego.96@gmail.com>
2021-05-22 18:01:19 +0200

    zoom + circular reveal image

--- app/src/main/java/com/mad/carpooling/ui/trip_details/TripDetailsFragment.kt
+++ app/src/main/java/com/mad/carpooling/ui/trip_details/TripDetailsFragment.kt
@@ -1,10 +1,18 @@
 package com.mad.carpooling.ui.trip_details
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.graphics.Point
+import android.graphics.Rect
+import android.graphics.RectF
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
 import android.view.*
+import android.view.animation.DecelerateInterpolator
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
@@ -39,6 +47,7 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
     private lateinit var trip: Trip
     private lateinit var ivCarPic: ImageView
     private lateinit var ivProfilePic: ImageView
+    private lateinit var ivExpanded: ImageView
     private lateinit var tvDepartureLocation: TextView
     private lateinit var tvDepartureDate: TextView
     private lateinit var tvDepartureTime: TextView
@@ -63,6 +72,16 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
     private var pets = false
     private var music = false
 
+    // Hold a reference to the current animator,
+    // so that it can be canceled mid-way.
+    private var currentAnimator: Animator? = null
+
+    // The system "short" animation time duration, in milliseconds. This
+    // duration is ideal for subtle animations or animations that occur
+    // very frequently.
+    private var shortAnimationDuration: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
@@ -72,6 +91,7 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
         super.onViewCreated(view, savedInstanceState)
 
         ivCarPic = view.findViewById(R.id.iv_tripDetails_car_pic)
+        ivExpanded = view.findViewById(R.id.expanded_image)
         ivProfilePic = view.findViewById(R.id.iv_tripDetails_profile_pic)
         tvDepartureLocation = view.findViewById(R.id.tv_tripDetails_departureLocation)
         tvDepartureDate = view.findViewById(R.id.tv_tripDetails_departureDate)
@@ -104,14 +124,6 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
 
         trip = newTripsMap[args.id]!!
 
-        Log.d("INFO", trip.owner!!.id)
-
-        // ivCarPic to be init from remote resource
-//        if (trip.imageCarURL != null) {
-//            BitmapFactory.decodeFile(trip.imageCarURL)?.also { bitmap ->
-//                ivCarPic.setImageBitmap(bitmap)
-//            }
-//        }
         if (trip.imageCarURL != "") {
             Glide.with(view).load(trip.imageCarURL).into(ivCarPic)
         }
@@ -126,6 +138,13 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
                 Glide.with(view).load(value?.get("imageUserRef"))
                     .into(ivProfilePic)
             }
+
+        ivProfilePic.setOnClickListener {
+            zoomImageFromThumb(ivProfilePic, trip.imageCarURL, view)
+        }
+        // Retrieve and cache the system's default "short" animation time.
+        shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
+
         tvDepartureLocation.text = trip.departure
         tvArrivalLocation.text = trip.arrival
         tvDepartureDate.text =
@@ -277,6 +296,156 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
                     bsb.state = BottomSheetBehavior.STATE_EXPANDED
                 else if (bsb.state == BottomSheetBehavior.STATE_EXPANDED)
                     bsb.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+        }
+    }
+
+    private fun zoomImageFromThumb(thumbView: View, imageResId: String?, view: View) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        currentAnimator?.cancel()
+
+        // Load the high-resolution "zoomed-in" image.
+        Glide.with(view).load(imageResId).into(ivExpanded)
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        val startBoundsInt = Rect()
+        val finalBoundsInt = Rect()
+        val globalOffset = Point()
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBoundsInt)
+            view.findViewById<View>(R.id.container)
+            .getGlobalVisibleRect(finalBoundsInt, globalOffset)
+        startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+        finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+
+        val startBounds = RectF(startBoundsInt)
+        val finalBounds = RectF(finalBoundsInt)
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        val startScale: Float
+        if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
+            // Extend start bounds horizontally
+            startScale = startBounds.height() / finalBounds.height()
+            val startWidth: Float = startScale * finalBounds.width()
+            val deltaWidth: Float = (startWidth - startBounds.width()) / 2
+            startBounds.left -= deltaWidth.toInt()
+            startBounds.right += deltaWidth.toInt()
+        } else {
+            // Extend start bounds vertically
+            startScale = startBounds.width() / finalBounds.width()
+            val startHeight: Float = startScale * finalBounds.height()
+            val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
+            startBounds.top -= deltaHeight.toInt()
+            startBounds.bottom += deltaHeight.toInt()
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.alpha = 0f
+        // get the center for the clipping circle
+        val cx = ivExpanded.width / 2
+        val cy = ivExpanded.height / 2
+
+        // get the final radius for the clipping circle
+        val finalRadius = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+        // create the animator for this view (the start radius is zero)
+        val anim = ViewAnimationUtils.createCircularReveal(ivExpanded, cx, cy, 0f, finalRadius)
+        // make the view visible and start the animation
+        ivExpanded.visibility = View.VISIBLE
+        anim.start()
+        //ivExpanded.visibility = View.VISIBLE
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        ivExpanded.pivotX = 0f
+        ivExpanded.pivotY = 0f
+
+
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        currentAnimator = AnimatorSet().apply {
+            play(
+                ObjectAnimator.ofFloat(
+                ivExpanded,
+                View.X,
+                startBounds.left,
+                finalBounds.left)
+            ).apply {
+                with(ObjectAnimator.ofFloat(ivExpanded, View.Y, startBounds.top, finalBounds.top))
+                with(ObjectAnimator.ofFloat(ivExpanded, View.SCALE_X, startScale, 1f))
+                with(ObjectAnimator.ofFloat(ivExpanded, View.SCALE_Y, startScale, 1f))
+            }
+            duration = shortAnimationDuration.toLong()
+            interpolator = DecelerateInterpolator()
+            addListener(object : AnimatorListenerAdapter() {
+
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimator = null
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                    currentAnimator = null
+                }
+            })
+            start()
+        }
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        ivExpanded.setOnClickListener {
+            currentAnimator?.cancel()
+
+            val cx = ivExpanded.width / 2
+            val cy = ivExpanded.height / 2
+
+            // get the initial radius for the clipping circle
+            val initialRadius = Math.hypot(cx.toDouble(), cy.toDouble()).toFloat()
+
+            // create the animation (the final radius is zero)
+            val anim = ViewAnimationUtils.createCircularReveal(ivExpanded, cx, cy, initialRadius, 0f)
+            anim.start()
+
+
+            // Animate the four positioning/sizing properties in parallel,
+            // back to their original values.
+            currentAnimator = AnimatorSet().apply {
+                play(ObjectAnimator.ofFloat(ivExpanded, View.X, startBounds.left)).apply {
+                    with(ObjectAnimator.ofFloat(ivExpanded, View.Y, startBounds.top))
+                    with(ObjectAnimator.ofFloat(ivExpanded, View.SCALE_X, startScale))
+                    with(ObjectAnimator.ofFloat(ivExpanded, View.SCALE_Y, startScale))
+                }
+                duration = shortAnimationDuration.toLong()
+                interpolator = DecelerateInterpolator()
+                addListener(object : AnimatorListenerAdapter() {
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        thumbView.alpha = 1f
+                        ivExpanded.visibility = View.GONE
+                        currentAnimator = null
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                        thumbView.alpha = 1f
+                        ivExpanded.visibility = View.GONE
+                        currentAnimator = null
+                    }
+                })
+                start()
             }
         }
     }
@@ -483,5 +652,4 @@ class StopAdapter(private val stops: ArrayList<String>?) :
     override fun getItemCount(): Int {
         return stops?.size ?: 0
     }
-
 }
